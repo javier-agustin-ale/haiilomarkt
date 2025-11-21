6696a7b0db401390d58a84439047a4adf6b51859
JavierAle <javieragustinale@gmail.com>
2025-11-21 11:41:06 +0100

    Create API structure

---
+++ backend/config.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+const dialect = process.env.DB_DIALECT || 'sqlite';
+
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect,
+		storage: dialect === 'sqlite' ? process.env.DB_STORAGE : undefined,
+		logging: false,
+	}
+);
+
+export default sequelize;
